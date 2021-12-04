bd650dd939ae89eab7befd66c1da64ca0da37cb2
pkien01 <phamkienxmas2001@gmail.com>
2021-12-04 14:09:53 -0600

    Elliptical orbit

--- solar_system_simulator.py
+++ solar_system_simulator.py
@@ -6,10 +6,11 @@ import random
 
 n = 10
 name_id = {'Sun': 0, 'Mercury':1, 'Venus': 2, 'Earth': 3, 'Mars': 4, 'Jupiter': 5, 'Saturn': 6,	'Uranus': 7, 'Neptune': 8, 'Pluto': 9}
-color = ['yellow', 'grey', 'light brown', 'green', 'light red', 'brown', 'golden brown', 'blue green', 'blue', 'tan']
 mass = [m*1e24 for m in [1.98847e6, 0.330, 4.87, 5.97, 0.642, 1898, 568, 86.8, 102, 0.0146]]
-dist = [d*1e9 for d in [0., 69.8, 108.9, 152.1, 249.2, 816.6, 1514.5, 3003.6, 4545.7, 7375.9]]
+aphelion = [d*1e9 for d in [0., 69.8, 108.9, 152.1, 249.2, 816.6, 1514.5, 3003.6, 4545.7, 7375.9]]
 diam = [1.3927e6, 4879, 12104, 12756, 6792, 142984, 120536, 51118, 49528, 2370]
+orbit_vel = [v0*1000. for v0 in [0., 38.86, 34.79, 29.29, 21.97, 12.44, 9.09, 6.49, 5.37, 3.71]]
+eccentricity = [0., 0.205, 0.007, 0.017, 0.094, 0.049, 0.057, 0.046, 0.011, 0.244]
 min_diam = min(diam)
 rel_sz = [math.floor(math.log(sz / min_diam))*3 + 1 for sz in diam]
 #print(rel_sz)
@@ -22,10 +23,12 @@ net_p = 0
 net_cm = 0
 for i in range(1, n):
     theta_r = random.uniform(0, math.pi*2)
-    pos[i,:] = np.array([dist[i]*math.cos(theta_r), dist[i]*math.sin(theta_r)])
+    dist = aphelion[i]
+    pos[i,:] = np.array([dist*math.cos(theta_r), dist*math.sin(theta_r)])
     net_cm += mass[i]*pos[i]
 
-    v_mag = math.sqrt(G*mass[0]/dist[i])
+    v_mag = orbit_vel[i]
+    #v_mag = math.sqrt(G*mass[0]/dist[i])
     theta_v = math.pi/2 + theta_r
     vel[i,:] = np.array([v_mag*math.cos(theta_v), v_mag*math.sin(theta_v)])
     net_p += mass[i]*vel[i]
@@ -35,9 +38,10 @@ vel[0,:] = -net_p/mass[0]
 
 fig = plt.figure()
 ax = fig.add_subplot(1, 1, 1)
-lim_D = max(dist)*1.1
+lim_D = max(aphelion)*1.1
 ax.set_xlim(-lim_D, lim_D)
 ax.set_ylim(-lim_D, lim_D)
+ax.set_aspect('equal', 'box')
 ax.set_xlabel('x (m)',fontsize=10)
 ax.set_ylabel('y (m)',fontsize=10)
 
@@ -67,7 +71,7 @@ def animate(frame, display_freq=80):
             omega = math.hypot(*vel[i])/math.hypot(*pos[i])
             sweep_angle[i] += omega*dt
             orbit[i].append(pos[i].copy())
-            if sweep_angle[i] > 2.*math.pi:
+            if sweep_angle[i] > 2.*math.pi*3:
                 orbit[i].pop(0)
            
     for i in range(n): 

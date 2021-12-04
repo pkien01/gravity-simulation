fcfcee339190d95f0686f247de37dd05cfba2327
pkien01 <phamkienxmas2001@gmail.com>
2021-12-03 21:26:31 -0600

    Add orbit line

--- solar_system_simulator.py
+++ solar_system_simulator.py
@@ -5,9 +5,10 @@ from matplotlib.animation import FuncAnimation, PillowWriter
 import random
 
 n = 10
-name_id = {'SUN': 0, 'MERCURY':1, 'VENUS': 2, 'EARTH': 3, 'MARS': 4, 'JUPITER': 5, 'SATURN': 6,	'URANUS': 7, 'NEPTUNE': 8, 'PLUTO': 9}
+name_id = {'Sun': 0, 'Mercury':1, 'Venus': 2, 'Earth': 3, 'Mars': 4, 'Jupiter': 5, 'Saturn': 6,	'Uranus': 7, 'Neptune': 8, 'Pluto': 9}
+color = ['yellow', 'grey', 'light brown', 'green', 'light red', 'brown', 'golden brown', 'blue green', 'blue', 'tan']
 mass = [m*1e24 for m in [1.98847e6, 0.330, 4.87, 5.97, 0.642, 1898, 568, 86.8, 102, 0.0146]]
-dist = [d*1e9 for d in [0., 46.0, 107.5, 147.1, 206.6, 740.5, 1352.6, 2741.3, 4444.5, 4436.8]]
+dist = [d*1e9 for d in [0., 69.8, 108.9, 152.1, 249.2, 816.6, 1514.5, 3003.6, 4545.7, 7375.9]]
 diam = [1.3927e6, 4879, 12104, 12756, 6792, 142984, 120536, 51118, 49528, 2370]
 min_diam = min(diam)
 rel_sz = [math.floor(math.log(sz / min_diam))*3 + 1 for sz in diam]
@@ -37,31 +38,53 @@ ax = fig.add_subplot(1, 1, 1)
 lim_D = max(dist)*1.1
 ax.set_xlim(-lim_D, lim_D)
 ax.set_ylim(-lim_D, lim_D)
-ax.set_xlabel('x-coordinate (m)',fontsize=10)
-ax.set_ylabel('y-coordinate (m)',fontsize=10)
+ax.set_xlabel('x (m)',fontsize=10)
+ax.set_ylabel('y (m)',fontsize=10)
 
-planet_plot = ax.scatter(pos[1:,0], pos[1:, 1],  s=rel_sz[1:])
-star_plot = ax.scatter(*pos[0], s=rel_sz[0])
+obj_plot = ax.scatter(pos[:,0], pos[:, 1],  s=rel_sz)
 
-def animate(frame):
-    for t in range(100):
+sweep_angle = np.zeros([n,])
+orbit = [[pos[i].copy(),] for i in range(n)]
+orbit_plot = [None]*n
+for i in range(n): 
+    cur_orbit = np.array(orbit[i])
+    orbit_plot[i], = ax.plot(cur_orbit[:,0], cur_orbit[:,1], linewidth=1.)
+
+ax.legend(orbit_plot, name_id.keys())
+def animate(frame, display_freq=80):
+    orbit_displayed = False
+    for t in range(display_freq):
         for i in range(n):
             net_acc = [0., 0.]
             for j in range(n):
                 if j != i: 
-                    Rij = pos[i] - pos[j]
-                    net_acc += -G*mass[j]*Rij*((Rij[0]**2 + Rij[1]**2)**-1.5)
+                    r_vec = pos[i] - pos[j]
+                    net_acc += -G*mass[j]*r_vec*((r_vec[0]**2 + r_vec[1]**2)**-1.5)
 
             vel[i] += net_acc*dt 
             pos[i] += vel[i]*dt
 
-    total_days = 100*frame
+            omega = math.hypot(*vel[i])/math.hypot(*pos[i])
+            sweep_angle[i] += omega*dt
+            orbit[i].append(pos[i].copy())
+            if sweep_angle[i] > 2.*math.pi:
+                orbit[i].pop(0)
+           
+    for i in range(n): 
+        #orbit[i].append(pos[i].copy())
+        #if sweep_angle[i] > 2.*math.pi:
+        #    orbit[i].pop(0)
+        cur_orbit = np.array(orbit[i])
+        orbit_plot[i].set_data(cur_orbit[:, 0], cur_orbit[:, 1])
+    
+
+    total_days = display_freq*frame
     ax.set_title("%d years, %d days" % (total_days//365, total_days%365))
     
-    planet_plot.set_offsets(pos[1:])
-    star_plot.set_offsets(pos[0])
-    return planet_plot, star_plot
+    obj_plot.set_offsets(pos)
+    return obj_plot
 
-anim = FuncAnimation(fig, animate, frames=None, interval=30, repeat=False)
-plt.show()
-#anim.save('solar_system.gif', fps=30)
+if __name__ == "__main__":
+    anim = FuncAnimation(fig, animate, frames=None, interval=30, repeat=False)
+    plt.show()
+    #anim.save('solar_system.gif', fps=30)
